5d89ec9b36b86844e207b4c66b274bdd6fefe91c
Ransu <96833940+Ransu1110@users.noreply.github.com>
2022-03-08 20:33:54 +0800

    JettWreck v. 1.1
    Update:
    added dash mechanic to the game

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ right = False
 left = False
 x = 500
 y = 500
-dash_time = 20
-dash_axis = 0
+isDash = False
+dash_power = 6
 window = pygame.display.set_mode((x, y))
 
 
@@ -21,7 +21,7 @@ class Player:
     def __init__(self, p_x, p_y):
         self.p_x = p_x
         self.p_y = p_y
-        self.player_speed = 5
+        self.player_speed = 10
 
     def draw(self, win):
         player_sprite = pygame.image.load("sprites/jett.png").convert_alpha()
@@ -42,10 +42,6 @@ def redraws():
     window.blit(bg, (0, 0))
     player.draw(window)
     enemy.draw(window)
-    if right:
-        window.blit(right_dash, (player.p_x, player.p_y))
-    if left:
-        window.blit(left_dash, (player.p_x, player.p_y))
 
     pygame.display.update()
 
@@ -55,7 +51,7 @@ player = Player(250, 400)
 enemy = Enemy(0, 60)
 run = True
 while run:
-    pygame.time.delay(10)
+    pygame.time.delay(30)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -70,36 +66,46 @@ while run:
     # S
     if key[pygame.K_s] and player.p_y < y:
         player.p_y += player.player_speed
-    # A
-    if key[pygame.K_a] and player.p_x > -1:
-        player.p_x -= player.player_speed
-        dash_axis = 1
-    # D
-    if key[pygame.K_d] and player.p_x < x - 64:
-        player.p_x += player.player_speed
-        dash_axis = 2
-    window.fill((0, 0, 0))
-    # Dash (test)
-    dash_time += 0.2
-    # timer limit
-    if dash_time > 20:
-        dash_time = 20
-    if dash_time >= 20:
-        # Right Dash
-        if key[pygame.K_e] and dash_axis == 2:
 
-            player.p_x += 25
-            dash_time = 0
-            right = True
-        else:
-            right = False
-
-        # Left Dash
-        if key[pygame.K_e] and dash_axis == 1:
-            player.p_x -= 25
-            dash_time = 0
+    if not isDash:
+        # A
+        if key[pygame.K_a] and player.p_x > -1:
+            player.p_x -= player.player_speed
             left = True
-        else:
+            right = False
+        # D
+        if key[pygame.K_d] and player.p_x < x - 64:
+            player.p_x += player.player_speed
+            right = True
             left = False
+    if left:
+        if not isDash:
+
+            # SPACE/Dash
+            if key[pygame.K_SPACE]:
+                isDash = True
+        else:
+            if dash_power >= 1:
+                player.p_x -= (dash_power ** 2)
+                dash_power -= 1
+            else:
+                isDash = False
+                dash_power = 6
+    if right:
+        if not isDash:
+
+            # SPACE/Dash
+            if key[pygame.K_SPACE]:
+                isDash = True
+        else:
+            if dash_power >= 0:
+                player.p_x += (dash_power ** 2)
+                dash_power -= 1
+            else:
+                isDash = False
+                dash_power = 6
+    window.fill((0, 0, 0))
+    print(player.p_x)
+    print(dash_power)
     redraws()
 pygame.quit()
